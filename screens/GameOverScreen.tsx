94f6e99ea714240ea059982861ad6edb02158c44
Hung Phan <phanhung1309@gmail.com>
2022-02-16 23:00:41 +0700

    Responsive GameOverScreen

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import {StyleSheet, Text, View, Image, Dimensions} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
 
 import MainButton from '../components/MainButton';
 import colors from '../styles/colors';
@@ -15,30 +22,60 @@ const GameOverScreen: React.FC<GameOverScreenProps> = ({
   userNumber,
   onRestart,
 }) => {
+  const [availableDeviceWidth, setAvailableDeviceWidth] = useState<number>(
+    Dimensions.get('window').width,
+  );
+  const [availableDeviceHeight, setAvailableDeviceHeight] = useState<number>(
+    Dimensions.get('window').height,
+  );
+
+  useEffect(() => {
+    const updateLayout = () => {
+      setAvailableDeviceWidth(Dimensions.get('window').width);
+      setAvailableDeviceHeight(Dimensions.get('window').height);
+    };
+
+    const subscription = Dimensions.addEventListener('change', updateLayout);
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>The Game is Over!</Text>
-      <View style={styles.imageContainer}>
-        <Image
-          style={styles.image}
-          source={require('../assets/images/success.png')}
-          // source={{
-          //   uri: 'https://cdn.pixabay.com/photo/2019/01/22/18/30/summit-3948706_960_720.jpg',
-          // }}
-          resizeMode="cover"
-        />
+    <ScrollView>
+      <View style={styles.screen}>
+        <Text style={styles.title}>The Game is Over!</Text>
+        <View
+          style={{
+            ...styles.imageContainer,
+            ...{
+              width: availableDeviceWidth * 0.7,
+              height: availableDeviceWidth * 0.7,
+              borderRadius: (availableDeviceWidth * 0.7) / 2,
+              marginVertical: availableDeviceHeight / 30,
+            },
+          }}>
+          <Image
+            style={styles.image}
+            source={require('../assets/images/success.png')}
+            // source={{
+            //   uri: 'https://cdn.pixabay.com/photo/2019/01/22/18/30/summit-3948706_960_720.jpg',
+            // }}
+            resizeMode="cover"
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <Text style={styles.resultText}>
+            Your phone needed{' '}
+            <Text style={styles.highlight}>{roundsNumber}</Text> rounds to guess
+            the number <Text style={styles.highlight}>{userNumber}</Text>
+          </Text>
+        </View>
+        <View style={{marginTop: availableDeviceHeight / 20}}>
+          <MainButton onPress={onRestart}>RESTART</MainButton>
+        </View>
       </View>
-      <View style={styles.resultContainer}>
-        <Text style={styles.resultText}>
-          Your phone needed <Text style={styles.highlight}>{roundsNumber}</Text>{' '}
-          rounds to guess the number{' '}
-          <Text style={styles.highlight}>{userNumber}</Text>
-        </Text>
-      </View>
-      <View style={styles.button}>
-        <MainButton onPress={onRestart}>RESTART</MainButton>
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -51,20 +88,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   imageContainer: {
-    width: Dimensions.get('window').width * 0.7,
-    height: Dimensions.get('window').width * 0.7,
-    borderRadius: (Dimensions.get('window').width * 0.7) / 2,
     borderWidth: 2,
     borderColor: 'black',
     overflow: 'hidden',
-    marginVertical: Dimensions.get('window').width / 20,
   },
   image: {
     width: '100%',
     height: '100%',
-  },
-  button: {
-    marginTop: Dimensions.get('window').width / 20,
   },
   title: {
     fontSize: 20,
